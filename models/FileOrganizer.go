8346a77f98274fc7f412301116454342f4447a38
Alex Luzhanovskyi <alex@sportsevents365.com>
2023-12-15 07:04:28 +0200

    Return err instead of bool, err

--- models/FileOrganizer.go
+++ models/FileOrganizer.go
@@ -8,7 +8,7 @@ func (fo *FileOrganizer) SetStrategy(strategy FileOperationStrategy) {
 	fo.Strategy = strategy
 }
 
-func (fo *FileOrganizer) ExecuteStrategy() (bool, error) {
+func (fo *FileOrganizer) ExecuteStrategy() error {
 	var fileManager *FileManager = &FileManager{}
 	return fo.Strategy.Execute(fileManager)
 }
